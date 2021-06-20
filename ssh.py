7414da5c655395aecdd7c344798b4569ebea7d74
alfogs0 <86094517+alfogs0@users.noreply.github.com>
2021-06-20 12:21:53 -0500

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,4 +1,2 @@
 import os
-os.system('systemctl status ssh')
-os.system('systemctl start ssh')
-os.system('ssh limitado@localhost') #Se ejecuta el proceso en segundo plano con nohup
+os.system('sudo su')
